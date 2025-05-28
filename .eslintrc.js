b0b02c39bfb36607ad8f426cd60692211ec18265
FP22FD <112474910+FP22FD@users.noreply.github.com>
2025-05-28 13:13:03 +0200

    chore: install and configure ESLint with perfectionist

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+import js from '@eslint/js'
+
+export default [
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            parser: typescript - eslint.parser,
+        },
+        plugins: {
+            perfectionist: eslintPluginPerfectionist,
+        },
+        rules: {
+            'perfectionist/sort-imports': 'error',
+        },
+    },
+    js.configs.recommended,
+]
