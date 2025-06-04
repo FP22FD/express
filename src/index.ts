cacb4de75bb54d05f7fe0310e6db54bc195094a8
FP22FD <112474910+FP22FD@users.noreply.github.com>
2025-06-04 13:44:06 +0200

    Move functions basepath

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,105 @@
 import dotenv from "dotenv";
-import express from "express";
+import express, { Router } from "express";
+import serverless from "serverless-http";
 
 dotenv.config();
-
 const app = express();
-const PORT = process.env.PORT ?? "3000";
+const router = Router();
 
-app.get("/", (_req, res) => {
+router.get("/", (_req, res) => {
   res.send(`Hello from Express + TypeScript! + ${process.env.TEST ?? ""}`);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+router.get("/hello", (_req, res) => {
+  res.send("Hello World!");
 });
+
+router.get("/books", (_req, res) => {
+  res.send([]);
+});
+
+// NO!
+// /api/GetBooksById?bookId=abc
+// /api/GetBooksByTitle?title=abc
+router.get("/GetBooksById", (req, res) => {
+  // lookup nel db e fetch the book by ID
+  const bookId = req.query.bookId;
+
+  // dummy response
+  res.send({
+    bookId: bookId,
+    title: "Sample Book Title",
+  });
+});
+
+// /api/books/:bookId
+router.get("/books/:bookId", (req, res) => {
+  // lookup nel db e fetch the book by ID
+  const bookId = req.params.bookId;
+
+  // dummy response
+  res.send({
+    bookId: bookId,
+    title: "Sample Book Title",
+  });
+});
+
+// /api/books/:bookId
+router.delete("/books/:bookId", (req, res) => {
+  // lookup nel db e fetch the book by ID
+  const bookId = req.params.bookId;
+  console.log(`Deleting book with ID: ${bookId}`);
+
+  // dummy response
+  res.send(true);
+});
+
+interface BookCreateRequest {
+  title: string;
+}
+
+// /api/books/:bookId
+router.post("/books", (req, res) => {
+  // lookup nel db e fetch the book by ID
+  const body = req.body as BookCreateRequest;
+  const title = body.title;
+  if (!title) {
+    res.status(400).send("Title is required");
+    return;
+  }
+
+  // dummy response
+  res.send(true);
+});
+
+interface BookEditRequest {
+  title: string;
+}
+
+// /api/books/:bookId
+router.put("/books/:bookId", (req, res) => {
+  // lookup nel db e fetch the book by ID
+  const body = req.body as BookEditRequest;
+  const title = body.title;
+  if (!title) {
+    res.status(400).send("Title is required");
+    return;
+  }
+
+  // dummy response
+  res.send(true);
+});
+
+app.use("/api/", router);
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+if (process.env.ENV === "local") {
+  const PORT = process.env.PORT ?? "3000";
+
+  app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
+}
+export const handler = serverless(app);
