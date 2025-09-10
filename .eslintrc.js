4837b6846e91d96d17d44de9d4d39ae5e1e10998
FP22FD <112474910+FP22FD@users.noreply.github.com>
2025-09-10 14:50:02 +0200

    chore: add project configuration files (ESLint, Prettier, TypeScript, Husky, CI, Vitest)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,31 @@
+// import js from "@eslint/js";
+
+// export default [
+//   {
+//     files: ["**/*.ts"],
+//     languageOptions: {
+//       parser: typescript - eslint.parser,
+//     },
+//     plugins: {
+//       perfectionist: eslintPluginPerfectionist,
+//     },
+//     rules: {
+//       "perfectionist/sort-imports": "error",
+//     },
+//   },
+//   js.configs.recommended,
+// ];
+
 import js from "@eslint/js";
+import { ESLint } from "eslint";
+import typescriptParser from "@typescript-eslint/parser";
+import eslintPluginPerfectionist from "eslint-plugin-perfectionist";
 
 export default [
   {
     files: ["**/*.ts"],
     languageOptions: {
-      parser: typescript - eslint.parser,
+      parser: typescriptParser,
     },
     plugins: {
       perfectionist: eslintPluginPerfectionist,
